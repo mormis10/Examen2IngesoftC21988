ef2926ebd026afd86de57d26c9ddce3c36580b08
mormis10 <cerdasignacio5@gmail.com>
2025-12-06 15:31:57 -0500

    Añadiendo los repositorios que van a interactuar con nuestra base de datos falsa

---
+++ Backend/ExamTwo/Repositories/CoffeeMachineRepository.cs
@@ -0,0 +1,49 @@
+﻿using ExamTwo.Controllers;
+using ExamTwo.Interfaces;
+
+namespace ExamTwo.Repositories
+{
+    public class CoffeeMachineRepository : ICoffeMachineRepository
+    {
+        private readonly Database? database;
+
+        public CoffeeMachineRepository(Database? database)
+        {
+            this.database = database;
+        }
+
+        public Dictionary<string, int> GetCoffeeStock()
+        {
+            return this.database.getAllCoffeeData();
+        }
+
+        public Dictionary<string, int> GetCoffeePrices()
+        {
+            return this.database.getCofeePrices();
+        }
+
+        public void UpdateCoffeeStock(string type, int newQuantity)
+        {
+
+        }
+
+        public Dictionary<int, int> GetCoinInventory()
+        {
+            return this.database.getChangeQuantity();
+        }
+
+
+
+        public bool UpdateCoffeeStatus(string name, int count)
+        {
+            return this.database.UpdateCoffeeStatus(name, count);
+        }
+
+        public bool UpdateCoinStauts(int coin, int count)
+        {
+            return this.database.UpdateCoinStatus(coin, count);
+
+        }
+
+    }
+}
