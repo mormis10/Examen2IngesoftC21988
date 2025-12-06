ea4fb6e6c244b727f6b492ae5d06925111e6abb3
mormis10 <cerdasignacio5@gmail.com>
2025-12-06 15:29:26 -0500

    Modelos que se va a encargar del mapeo de datos durante el intercambio de información entre el backedn y el frontend

---
+++ Backend/ExamTwo/Models/PaymentModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExamTwo.Models
+{
+    public class PaymentModel
+    {
+        public int TotalAmount { get; set; }
+        public List<int>? Coins { get; set; }
+        public List<int>? Bills { get; set; }
+    }
+}
---
+++ Backend/ExamTwo/Models/TransactionResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ExamTwo.Models
+{
+    public class TransactionResultModel
+    {
+        public bool Success { get; set; }
+        public int ChangeAmount { get; set; }
+        public Dictionary<int, int>? ChangeBreakdown { get; set; }
+        public string? Message { get; set; }
+    }
+}
